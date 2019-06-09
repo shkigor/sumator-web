6c2906f439e99bb748f2fed83153c43dfe594f66
Igor Shklyar <igor.shklyar@gmail.com>
2019-06-09 15:38:51 +0300

    Add dependency jackson-core from https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core/2.9.8 https://bintray.com/bintray/jcenter/com.fasterxml.jackson.core%3Ajackson-core and jackson simple example

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import model.Person;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,13 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "Hello from Spring Boot!!!";
+    }
+
+    @RequestMapping("/json")
+    public Person json() {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("Сергей Иванович");
+        return person;
     }
 }
